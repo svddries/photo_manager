be86aa62c21623c0d0250f2b445098d4768dcd7b
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-09-04 21:48:59 +0200

    Can select gui or scan mode from command-line

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,15 @@
 
 int main(int argc, char **argv)
 {
-    if (argc < 3)
+    if (argc < 4)
     {
-        std::cout << "usage: photo_manager [DATABASE FILE] [IMAGE DIRECTORY]" << std::endl;
+        std::cout << "usage: photo_manager [COMMAND] [DATABASE FILE] [IMAGE DIRECTORY]" << std::endl;
         return 1;
     }
 
-    std::string database_filename = argv[1];
-    std::string image_dir = argv[2];
+    std::string command = argv[1];
+    std::string database_filename = argv[2];
+    std::string image_dir = argv[3];
 
     PhotoDatabase db;
     loadDatabase(database_filename, db);
@@ -33,65 +34,75 @@ int main(int argc, char **argv)
 //        cv::waitKey();
 //    }
 
-    GUI gui(&db);
-    gui.run();
+    if (command == "gui")
+    {
+        GUI gui(&db);
+        gui.run();
+    }
+    else if (command == "scan")
+    {
+        boost::filesystem::path p(image_dir);
+        boost::filesystem::recursive_directory_iterator it_dir(p);
+        boost::filesystem::recursive_directory_iterator end;
 
-//    boost::filesystem::path p(image_dir);
-//    boost::filesystem::recursive_directory_iterator it_dir(p);
-//    boost::filesystem::recursive_directory_iterator end;
+        std::set<std::string> photo_exts;
+        photo_exts.insert(".jpg");
+        photo_exts.insert(".png");
+        photo_exts.insert(".jpeg");
 
-//    std::set<std::string> photo_exts;
-//    photo_exts.insert(".jpg");
-//    photo_exts.insert(".png");
-//    photo_exts.insert(".jpeg");
+        std::set<std::string> found_exts;
 
-//    std::set<std::string> found_exts;
+        int i_photo = 0;
+        while(it_dir != end)
+        {
+            if (boost::filesystem::is_regular_file(*it_dir))
+            {
+                if (it_dir->path().filename().string()[0] != '.')
+                {
+                    std::string ext = it_dir->path().extension().string();
+                    std::transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
 
-//    int i_photo = 0;
-//    while(it_dir != end)
-//    {
-//        if (boost::filesystem::is_regular_file(*it_dir))
-//        {
-//            if (it_dir->path().filename().string()[0] != '.')
-//            {
-//                std::string ext = it_dir->path().extension().string();
-//                std::transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
+                    found_exts.insert(ext);
 
-//                found_exts.insert(ext);
+                    if (photo_exts.find(ext) != photo_exts.end())
+                    {
+                        std::string abs_filename = it_dir->path().string();
+                        std::string rel_filename = abs_filename.substr(image_dir.size() + 1);
+                        std::string msum = md5sum(abs_filename);
 
-//                if (photo_exts.find(ext) != photo_exts.end())
-//                {
-//                    std::string abs_filename = it_dir->path().string();
-//                    std::string rel_filename = abs_filename.substr(image_dir.size() + 1);
-//                    std::string msum = md5sum(abs_filename);
+                        PhotoData* p = db.findPhoto(msum);
+                        if (!p)
+                        {
+                            // New photo
+                            std::cout << "New photo: " << rel_filename << std::endl;
+                            p = db.addPhoto();
+                            p->md5sum = msum;
+                            p->rel_filename = rel_filename;
+                            db.registerPhoto(p);
+                        }
+                        else
+                        {
+                            // Old photo, update filename
+                            p->rel_filename = rel_filename;
+                        }
 
-//                    PhotoData* p = db.findPhoto(msum);
-//                    if (!p)
-//                    {
-//                        // New photo
-//                        std::cout << "New photo: " << rel_filename << std::endl;
-//                        p = db.addPhoto();
-//                        p->md5sum = msum;
-//                        p->rel_filename = rel_filename;
-//                        db.registerPhoto(p);
-//                    }
-//                    else
-//                    {
-//                        // Old photo, update filename
-//                        p->rel_filename = rel_filename;
-//                    }
+                        ++i_photo;
+                        if (i_photo % 100 == 0)
+                            std::cout << i_photo << " photos scanned" << std::endl;
 
-//                    ++i_photo;
-//                    if (i_photo % 100 == 0)
-//                        std::cout << i_photo << " photos scanned" << std::endl;
+                        //                    std::cout << md5sum(abs_filename) << ": " << abs_filename << std::endl;
+                    }
+                }
+            }
 
-////                    std::cout << md5sum(abs_filename) << ": " << abs_filename << std::endl;
-//                }
-//            }
-//        }
-
-//        ++it_dir;
-//    }
+            ++it_dir;
+        }
+    }
+    else
+    {
+        std::cout << "Unknown command: " << command << std::endl;
+        return 0;
+    }
 
     writeDatabase(db, database_filename);
 
